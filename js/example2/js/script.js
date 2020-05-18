3290065948e8b64e7099d1e02f0fde32ae4fb167
Nazar Shcherbiy <Chay23@users.noreply.github.com>
2020-05-18 14:29:35 +0300

    Module-4-solution

---
+++ js/example2/js/script.js
@@ -0,0 +1,14 @@
+(function(window) {
+ 
+  var obj = {};
+
+  obj.dreamOn = function () {
+   console.log("I want to see the global scope! Let me out!");
+  };
+
+  window.doer = obj;
+ 
+ })(window);
+
+doer.dreamOn();
+doer.dreamOn();
